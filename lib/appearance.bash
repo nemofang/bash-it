1b5313d690b64bf7cc3fada7e77d74d25b5fe61d
Nemo Fang <fyj123121@gmail.com>
2015-12-24 15:17:22 +0800

    Cleanup some unused thing.

--- lib/appearance.bash
+++ lib/appearance.bash
@@ -4,9 +4,6 @@
 alias grep='grep --color=auto'
 export GREP_COLOR='1;33'
 
-# colored ls
-# export LSCOLORS='Gxfxcxdxdxegedabagacad'
-
 # here's LS_COLORS
 # github.com/trapd00r/LS_COLORS
 command -v gdircolors >/dev/null 2>&1 || alias gdircolors="dircolors"
